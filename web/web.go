e4b0d50be4cbaedde72106183ad44c0cd7e22d4e
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-02-14 04:02:51 +0000

    Made port configurable.

--- web/web.go
+++ web/web.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/siuyin/dflt"
 )
 
 // Serve launces a web server.
 func Serve() {
 	go func() {
+		port := dflt.EnvString("PORT", ":8080")
 		log.Println("webServer starting")
 		http.HandleFunc("/ans", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "The answer is 42.")
@@ -16,6 +19,6 @@ func Serve() {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Hello, this is from a go webserver running in kubernetes.")
 		})
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		log.Fatal(http.ListenAndServe(port, nil))
 	}()
 }
