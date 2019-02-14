bdbfe6f328bb7143a3a77bc03bb40e03baa3d481
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-02-14 02:27:38 +0000

    Added docs to main.go

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 
 func main() {
 
-	pub.Work()
-	web.Serve()
-	work.Work()
-	pipe.Work()
+	pub.Work()  // publish/subscribe system using nats streaming
+	web.Serve() // a simple web server in go
+	work.Work() // a worker system with aBoss and one or more workers
+	pipe.Work() // a pipeline work system
 
+	// good old main-line code
 	g := dflt.EnvString("greet", "hello")
 	for {
 		fmt.Println("skaffold-hello", g)
